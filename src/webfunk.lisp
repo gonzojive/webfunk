e431006f5f73dc1dab60b45fff8c5904981b732d
Red Daly <red@floatopian.com>
2009-04-20 19:02:08 -0700

    pretty usuable webfunk library

--- src/webfunk.lisp
+++ src/webfunk.lisp
@@ -1,13 +1,21 @@
 (defpackage :webfunk
   (:use :common-lisp)
-  (:export #:*web-packages*
-	   #:*web-package*
-	   #:web-package
-	   #:find-web-package
-	   #:web-defun #:web-defpackage #:webpackage
-	   #:in-web-package
-	   #:web-function
-	   #:webfunk-hunchentoot-dispatcher))
+  (:export 
+   ;; macros
+   #:web-defun
+   ;; web packages
+   #:web-defpackage #:webpackage
+   #:*web-packages*
+   #:*web-package*
+   #:web-package
+   #:find-web-package
+   #:in-web-package
+   #:web-package-name
+   #:web-package-functions
+   ;; web functions
+   #:web-function
+   ;; hunchentoot plug-in
+   #:webfunk-hunchentoot-dispatcher))
 
 (in-package :webfunk)
 
@@ -19,6 +27,9 @@
 
 (defvar *web-package* nil
   "Active web package.  Set by IN-WEB-PACKAGE.")
+
+(defvar *lisp-package-web-package-alist* nil
+  "AList to map lisp packages to web packages (many to one).")
 
 (defmacro in-web-package (web-package-designator)
   `(eval-when (:compile-toplevel :load-toplevel :execute)
@@ -64,7 +75,7 @@ Otherwise returns NIL"))
 
 (defmethod print-object ((object web-package) stream)
   (print-unreadable-object (object stream :type t)
-    (format stream (web-package-name object))))
+    (format stream "~A" (web-package-name object))))
 
 (defun find-web-package (name)
   "Finds the WEB-PACKAGE of the given name."
@@ -73,7 +84,7 @@ Otherwise returns NIL"))
 
 (defclass web-function (standard-generic-function)
   ((web-package :accessor web-package :initform nil :initarg :web-package)
-   (web-vars :accessor web-function-vars :initform nil :initarg :vars))
+   (web-param :accessor web-function-parameters :initform nil :initarg :web-parameters))
   (:metaclass closer-mop:funcallable-standard-class)
   (:documentation "A function designed to be called over the web via webfunk."))
 
@@ -91,33 +102,37 @@ Otherwise returns NIL"))
 ;  ()
 ;  (:documentation "web-generic-function-"))
 
-(defclass web-function-var ()
+(defclass web-function-parameter ()
   ((symbol
     :type symbol
     :initform nil
     :initarg :name :initarg :symbol
-    :accessor var-symbol :accessor var-name
-    :documentation "Canonical name of the function")
-   (parameter-name 
+    :accessor parameter-symbol :accessor parameter-name
+    :documentation "Canonical name of the argument")
+   (uri-name 
     :type (or string null)
     :initform nil
-    :initarg :parameter-name
-    :accessor var-parameter-name
+    :initarg :uri-name
+    :accessor parameter-uri-name
     :documentation "The string that names this variable in a URL.")
    (parameter-type
     :initform nil
     :initarg :parameter-type
-    :accessor var-parameter-type
+    :accessor parameter-type
     :documentation "It's unclear exctly what this is at the moment.")
-   (parameter-string-converter
-    :documentation "Function that will convert a string before passing it to this function."
-    :initform #'identity)
+;   (parameter-string-converter
+;    :documentation "Function that will convert a string before passing it to this function."
+;    :initform #'identity)
    (init-form-function
     :initform #'(lambda () nil)
     :type (or function null)
     :initarg :initform-function
-    :accessor var-init-form-function))
-  (:documentation "A variable definition in a web-function."))
+    :accessor parameter-init-form-function))
+  (:documentation "A parameter definition in a web-function."))
+
+(defmethod print-object ((object web-function-parameter) stream)
+  (print-unreadable-object (object stream :type t)
+    (format stream "~S" (parameter-name object))))
 
 (defmethod initialize-instance ((package web-package) &rest rest &key name (uri nil uri-key-p) &allow-other-keys)
   "Initializer for web package.  If URI is not set, sets it to downcased NAME + '/'"
@@ -129,22 +144,25 @@ Otherwise returns NIL"))
 	     :uri (format nil "~A/" (string-downcase name)) ;(web-package-name package)))
 	     rest)))
 
+(defmethod shared-initialize ((package web-package) slot-names &rest rest &key name (uri nil uri-key-p) lisp-packages &allow-other-keys)
+  "Initializer for web package.  If URI is not set, sets it to downcased NAME + '/'"
+  (declare (ignore uri))
+  (Format t "Lisp packages: ~S~%Rest: ~S~%" lisp-packages rest)
+  (if uri-key-p
+      (call-next-method)
+      (apply #'call-next-method package slot-names
+	     :uri (format nil "~A/" (string-downcase name)) ;(web-package-name package)))
+	     rest)))
+
 (defgeneric reinitialize-web-package (web-package &key)
   (:documentation "Called when WEB-DEFPACKAGE defines a package that already
 exists and the information must be SETF into the new package.")
   (:method ((package web-package) &rest rest
-	     &key (uri nil uri-supplied-p)
-	          name uri-aliases package-class
+	     &key package-class
 	    &allow-other-keys)
     (if (and package-class (not (eql package-class (class-of package))))
 	(apply #'change-class package package-class rest)
-	(progn
-	  (setf (web-package-uri package)
-		(if uri-supplied-p
-		    uri
-		    (format nil "/~A/" (string-downcase name))))
-	  (setf (web-package-uri-aliases package) uri-aliases)
-	  package))))
+	(apply #'reinitialize-instance package rest))))
 
 (defgeneric web-package-class-initarg-singular-p (web-package-class initarg)
   (:documentation "Returns T if the keyword option INITARG in a defpackage form
@@ -175,39 +193,50 @@ OPTION ::= (:uri STRING) | (:uri-aliases STRING*) | (:package-class package-clas
 					   option)))
 		     options)))
       (let ((%package (gensym "WEB-PACKAGE"))
-	    (package-name (string defined-package-name)))
+	    (package-name (if (stringp defined-package-name)
+			      (intern (string-upcase defined-package-name))
+			      defined-package-name)))
 	`(eval-when (:compile-toplevel :load-toplevel :execute)
 	   (let ((,%package (make-web-package ,web-package-class
 					      :name ,package-name
 					      ,@(options-to-make-instance-keys options))))
-	     (pushnew ,%package *web-packages*)
 	     ,%package))))))
 
 (defmacro webpackage (defined-package-name &body options)
   `(web-defpackage defined-package-name ,@options))
 
-(defun make-web-package (web-package-class &rest rest &key name &allow-other-keys)
+(defun make-web-package (web-package-class &rest rest &key name lisp-packages &allow-other-keys)
   "Creates a web package as if by MAKE-INSTANCE, except it will
 not instantiate a new package if one was found with the same name.
 In that case, new information will be SETF into the existing
 WEB-PACKAGE class by calling REINITIALIZE-WEB-PACKAGE with
 all the arguments passed to this function."
 ;  (format t "WEB-PACKAGE-PARAMS: ~S~%" rest)
-  (let ((web-package (find-web-package name)))
-    (values
-     (if (null web-package)
-	 (apply #'make-instance web-package-class rest)
-	 (apply #'reinitialize-web-package web-package rest))
-     (null web-package))))
+  (declare (optimize debug))
+  (format t "Args to make-web-package: ~S~%" rest)
+  (let ((web-package-maybe (find-web-package name)))
+    (let ((web-package (if (null web-package-maybe)
+			   (apply #'make-instance web-package-class rest)
+			   (apply #'reinitialize-web-package web-package-maybe rest))))
 
-(defmethod initialize-instance :after ((var web-function-var) &key &allow-other-keys)
+      (setf *web-packages* (pushnew web-package *web-packages*));(remove web-package-maybe *web-packages*)))
+      
+      (dolist (lisp-package lisp-packages)
+	(let ((package (find-package lisp-package)))
+	  (pushnew (cons package web-package) *lisp-package-web-package-alist*)))
+
+      (values
+	web-package
+	(null web-package-maybe)))))
+
+(defmethod initialize-instance :after ((param web-function-parameter) &key &allow-other-keys)
   "Set PARAMETER-NAME to downcased string of the variable's name and
 INIT-FORM-FUNCTION"
   ;; initialize the URL parameter name to the downcased version of
   ;; the variable name string
-  (when (null (var-parameter-name var))
-    (setf (var-parameter-name var)
-	  (string-downcase (symbol-name (var-name var))))))
+  (when (null (parameter-uri-name param))
+    (setf (parameter-uri-name param)
+	  (string-downcase (symbol-name (parameter-name param))))))
 
 (defmacro web-defun (description web-lambda-list &body body)
 ;;   "Defines a function intended to be called over the web (though it can be called
@@ -220,26 +249,38 @@ INIT-FORM-FUNCTION"
 ;; (var &key init-form parameter-name parameter-type)
 ;; 
 ;; description := name | name-lambda-list."
+  
+  ;; Standardize the description argument into the list form
+  (when (atom description) (setf description (list description)))
+  ;; Standardize the web-lambda-list items into lists
+  (setf web-lambda-list (mapcar #'(lambda (x) (if (atom x) (list x) x)) web-lambda-list))
   (let* ((fn-name (if (consp description) (first description) description))
-	 (vars
-	  (mapcar #'(lambda (var-form)
-		      (when (atom var-form)
-			(setf var-form (list var-form)))
-		      (destructuring-bind (name &key initform parameter-name parameter-type)
-			  var-form
-			(declare (ignore initform parameter-name parameter-type))
-			(let ((var (make-instance 'web-function-var
-						  :name name)))
-			  var)))
-		  web-lambda-list))
-	 (generic-lambda-list (cons '&key (mapcar 'var-name vars)))
-	 (method-lambda-list (cons '&key (mapcar 'var-name vars)))
+	 (parameter-names (mapcar #'first web-lambda-list))
+	 (make-parameters-form
+	  `(list ,@(mapcar #'(lambda (param-form)
+			       (destructuring-bind (name &key initform parameter-name parameter-type)
+				   param-form
+				 (declare (ignore initform parameter-name))
+				 `(make-instance 'web-function-parameter
+						 :name ',name
+						 :parameter-type ,parameter-type)))
+			   web-lambda-list)))
 
-	 (%fn (gensym "function")))
+	 (generic-lambda-list (cons '&key parameter-names))
+	 (method-lambda-list (cons '&key parameter-names))
+
+	 (%fn (gensym "function"))
+	 (%parameters (gensym "parameters")))
     `(eval-when (:compile-toplevel :load-toplevel :execute)
-       (let ((,%fn
-	      (defgeneric ,fn-name ,generic-lambda-list 
-		(:generic-function-class web-function))))
+       (let* ((*web-package* (or *web-package*
+				 (cdr (assoc *package* *lisp-package-web-package-alist*))))
+	      (,%parameters ,make-parameters-form)
+	      (,%fn
+	       (defgeneric ,fn-name ,generic-lambda-list 
+		 (:generic-function-class web-function))))
+
+	 (setf (web-function-parameters ,%fn) ,%parameters)
+
 	 (defmethod ,fn-name ,method-lambda-list
 	   ,@body)
 	 (pushnew ,%fn (web-package-functions *web-package*))
@@ -248,7 +289,8 @@ INIT-FORM-FUNCTION"
 
 (defun string-prefixp (string prefix)
   "Returns T if STRING begins with the same characters as are in prefix."
-  (string= string prefix :end1 (length prefix)))
+  (and (>= (length string) (length prefix))
+       (string= string prefix :end1 (length prefix))))
 
 (defmethod web-package-prefix-matches-uri? ((web-package web-package) uri)
   "Returns 2 values.  1. nil if does not match, the matched prefix if it does match.
@@ -286,8 +328,17 @@ INIT-FORM-FUNCTION"
 ;	 (when (not ,symbol-existed-p-var)
 ;	   (unintern ,var 
 
+(defparameter *wiretap* *standard-output*)
+
 (defmethod web-function-transform-request-into-arguments ((fn web-function) request)
-  `(:etc ,(hunchentoot:get-parameters request)))
+  (mapcan #'(lambda (fn-param)
+	      (list (intern (symbol-name (parameter-name fn-param)) :keyword)
+		    (compute-parameter (parameter-uri-name fn-param)
+				       (or (parameter-type fn-param) 'string)
+				       :both)))
+	  (web-function-parameters fn)))
+    
+
 
 (defmethod web-function-call-with-request ((fn web-function) request)
   (apply fn (web-function-transform-request-into-arguments fn request)))
@@ -311,7 +362,6 @@ INIT-FORM-FUNCTION"
 	(funcall fn)
 	"nobody handled request!")))
 
-
 (defun webfunk-hunchentoot-dispatcher (request)
   (or (loop :for web-package :in *web-packages*
 	 :when (web-package-handles-request? web-package request)
@@ -334,7 +384,6 @@ WEB-DEFUN, if there is one."
 ;		     (t (funcall uri request))))
 ;     :do (return easy-handler)))
 
-#+nil
 (defun make-keyword (string &key (destructivep nil))
   "Interns the upcased version of STRING into the KEYWORD package.
 Uses NSTRING-UPCASE if DESTRUCTIVEP is true.  Returns NIL if STRING is
@@ -344,7 +393,6 @@ not a string."
                  (nstring-upcase string)
                  (string-upcase string)) :keyword)))
 
-#+nil
 (defun convert-parameter-string (param-string param-type)
   "Converts the string PARAM-STRING to PARAM-TYPE where PARAM-TYPE is one of the
 symbols STRING, CHARACTERS, INTEGER, KEYWORD, or BOOLEAN - or
@@ -364,13 +412,11 @@ NIL unconditionally."
     (boolean t)
     (otherwise (error "Unsupported parameter conversion type: ~A" param-type))))
 
-#+nil
 (defun compute-simple-parameter (parameter-name type parameter-reader)
   "Retrieves the parameter named PARAMETER-NAME using the reader
 PARAMETER-READER and converts it to TYPE."
   (convert-parameter-string (funcall parameter-reader parameter-name) type))
 
-#+nil
 (defun compute-list-parameter (parameter-name type parameters)
   "Retrieves all parameters from PARAMETERS which are named
 PARAMETER-NAME, converts them to TYPE, and returns a list of
@@ -379,7 +425,6 @@ them."
         when (string= name parameter-name)
         collect (convert-parameter-string value type)))
 
-#+nil
 (defun compute-array-parameter (parameter-name type parameters)
   "Retrieves all parameters from PARAMETERS which are named like
 \"PARAMETER-NAME[N]\" \(where N is a non-negative integer),
@@ -389,7 +434,7 @@ is the corresponding value."
   #+:sbcl (declare (sb-ext:muffle-conditions warning))
   (let* ((index-value-list
           (loop for (full-name . value) in parameters
-                for index = (register-groups-bind (name index-string)
+                for index = (cl-ppcre:register-groups-bind (name index-string)
                                 ("^(.*)\\[(\\d+)\\]$" full-name)
                               (when (string= name parameter-name)
                                 (parse-integer index-string)))
@@ -403,7 +448,6 @@ is the corresponding value."
           do (setf (aref array index) value))
     array))
 
-#+nil
 (defun compute-hash-table-parameter (parameter-name type parameters key-type test-function)
   "Retrieves all parameters from PARAMETERS which are named like
 \"PARAMETER-NAME{FOO}\" \(where FOO is any sequence of characters
@@ -413,7 +457,7 @@ corresponding value is associated with the key FOO \(converted to
 KEY-TYPE)."
   (let ((hash-table (make-hash-table :test test-function)))
     (loop for (full-name . value) in parameters
-          for key = (register-groups-bind (name key-string)
+          for key = (cl-ppcre:register-groups-bind (name key-string)
                         ("^(.*){([^{}]+)}$" full-name)
                       (when (string= name parameter-name)
                         (convert-parameter-string key-string key-type)))
@@ -422,7 +466,6 @@ KEY-TYPE)."
                    (convert-parameter-string value type)))
     hash-table))
 
-#+nil
 (defun compute-parameter (parameter-name parameter-type request-type)
   "Computes and returns the parameter\(s) called PARAMETER-NAME
 and converts it/them according to the value of PARAMETER-TYPE.
@@ -430,14 +473,14 @@ REQUEST-TYPE is one of :GET, :POST, or :BOTH."
   (when (member parameter-type '(list array hash-table))
     (setq parameter-type (list parameter-type 'string)))
   (let ((parameter-reader (ecase request-type
-                              (:get #'get-parameter)
-                              (:post #'post-parameter)
-                              (:both #'parameter)))
+                              (:get #'hunchentoot:get-parameter)
+                              (:post #'hunchentoot:post-parameter)
+                              (:both #'hunchentoot:parameter)))
         (parameters (and (listp parameter-type)
                          (case request-type
-                           (:get (get-parameters))
-                           (:post (post-parameters))
-                           (:both (append (get-parameters) (post-parameters)))))))
+                           (:get (hunchentoot:get-parameters))
+                           (:post (hunchentoot:post-parameters))
+                           (:both (append (hunchentoot:get-parameters) (hunchentoot:post-parameters)))))))
     (cond ((atom parameter-type)
            (compute-simple-parameter parameter-name parameter-type parameter-reader))
           ((and (null (cddr parameter-type))
