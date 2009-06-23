cd5f396edc132fd480bb945633e10280ba30f1ac
Red Daly <red@condi.Stanford.EDU>
2009-06-23 09:11:02 -0700

    added static handler.

--- src/webfunk.lisp
+++ src/webfunk.lisp
@@ -14,6 +14,13 @@
    #:web-package-functions
    ;; web functions
    #:web-function
+
+   #:web-function-href
+   #:href
+
+   ;; serving files
+   #:serve-static-file
+   
    ;; hunchentoot plug-in
    #:webfunk-hunchentoot-dispatcher))
 
@@ -67,6 +74,12 @@ Otherwise returns NIL"))
     :initarg :uri-aliases
     :accessor web-package-uri-aliases
     :documentation "Secondary URIs used to locate the web package.")
+   (root-function-name
+    :type (or null symbol)
+    :initform nil
+    :initarg :root-function :initarg :root-function-name
+    :accessor web-package-root-function-name
+    :documentation "When the user navigates to the root a directory, this is the function name to call.")
    (functions
     :initform nil
     :initarg :functions
@@ -81,6 +94,10 @@ Otherwise returns NIL"))
   "Finds the WEB-PACKAGE of the given name."
   (when (not (stringp name)) (setf name (string name)))
   (find name *web-packages* :key #'web-package-name :test #'string-equal))
+
+(defun find-web-function (web-package fn-name)
+  "Returns the web function with the given symbol as its name in the given web package"
+  (find fn-name (web-package-functions web-package) :key #'web-function-name))
 
 (defclass web-function (standard-generic-function)
   ((web-package :accessor web-package :initform nil :initarg :web-package)
@@ -170,8 +187,43 @@ should be handed to (make-instance WEB-PACKAGE-CLASS) as a single value,
 rather than a list.")
   (:method ((web-package-class (eql (find-class 'web-package))) initarg)
     (case initarg
-      ((or :uri :listed) t)
+      ((or :uri :listed :root-function-name :root-function) t)
       (t nil))))
+
+(defgeneric web-package-class-initarg-evaluated-p (web-package-class initarg)
+  (:documentation "Returns T if the keyword option INITARG in a defpackage form
+should be handed to (make-instance WEB-PACKAGE-CLASS) evaluated or not evaluated.")
+  (:method ((web-package-class (eql (find-class 'web-package))) initarg)
+    (case initarg
+      ((or :uri :listed :root-function-name :root-function) nil)
+      (t nil))))
+
+(defun maphash-ret (fn hash-table)
+  (let ((result nil))
+    (maphash #'(lambda (a b)
+		 (push (funcall fn a b) result))
+	     hash-table)
+    (nreverse result)))
+		 
+
+(defun merge-options-by-keyword (options &optional (return-type 'list))
+  "Given a list of option forms, which generally looks like ((:a 1 2 3 4) (:b t) (:c 3) (:a 5)),
+merges all the options with the same car together.  So it will return something like
+ ((:a 1 2 3 4 5) (:b t)).  "
+  (let ((hash (make-hash-table))
+	(options-in-order nil))
+
+    (map nil #'(lambda (option)
+		 (setf options-in-order (concatenate 'list options-in-order (rest option)))
+		 (setf (gethash (car option) hash)
+		       (concatenate 'list (gethash (car option) hash) (rest option))))
+	 options)
+    (values (case return-type
+	      (list (apply #'concatenate 'list (maphash-ret #'list hash)))
+	      (hash-table hash)
+	      (t nil))
+	    options-in-order)))
+
 
 (defmacro web-defpackage (defined-package-name &body options)
   "Defines a WEB-PACKAGE.  Options is a list of options.  An option takes the form
@@ -181,17 +233,30 @@ OPTION ::= (:uri STRING) | (:uri-aliases STRING*) | (:package-class package-clas
 :uri-aliases specifies a list of alternative URIs that also identify this package
 :package-class designates the class this macro will instantiate, default is WEB-PACKAGE."
   (let ((web-package-class
-	 (find-class (or (find :package-class options :key #'car)
+	 (find-class (or (second (find :package-class options :key #'car))
 			 'web-package))))
     (flet ((options-to-make-instance-keys (options)
+	     ;; We have to be careful so that things evaluate in the right order.
+	     ;; basically option arguments should be evaluated in the order
+	     ;; they are seen.
 	     (mapcan #'(lambda (option)
+			 (let ((singular (web-package-class-initarg-singular-p web-package-class (car option)))
+			       (evaluated (web-package-class-initarg-evaluated-p web-package-class (car option))))
 			 (list (car option)
-			       `',(funcall (if (web-package-class-initarg-singular-p
-						web-package-class (car option))
-					       #'second
-					       #'rest)
-					   option)))
-		     options)))
+			       (if singular
+				   (if evaluated (second option) `',(second option))
+				   (if evaluated `(list ,@(rest option)) `'(,@(rest option)))))))
+				       
+					    
+
+				      
+			       
+;			       `',(funcall (if (web-package-class-initarg-singular-p
+;						web-package-class (car option))
+;					       #'second
+;					       #'rest)
+;					   option)))
+			 options)))
       (let ((%package (gensym "WEB-PACKAGE"))
 	    (package-name (if (stringp defined-package-name)
 			      (intern (string-upcase defined-package-name))
@@ -238,53 +303,86 @@ INIT-FORM-FUNCTION"
     (setf (parameter-uri-name param)
 	  (string-downcase (symbol-name (parameter-name param))))))
 
+(defvar *content-type-keyword-string-alist*
+  `((:html . "text/html")
+    (:css . "text/css")
+    (:javascript . "text/javascript")
+    (:jpeg . "image/jpeg")
+    (:gif . "image/gif")
+    (:png . "image/png")))
+    
+
+(defun stringify-content-type (content-type)
+  "Makes a content type string from content-type, which is either a string
+or a keyword symbol.
+
+:html -> text/html
+:css  -> text/css
+:javascript -> text/javascript"
+  (typecase content-type
+    (string content-type)
+    (symbol (if (keywordp content-type)
+		(cdr (assoc content-type *content-type-keyword-string-alist*))
+		(error "non-keyword symbol")))
+    (t (error "only accepts keywords and strings but got ~S" content-type))))
+
+(defun sane-web-package ()
+  (or *web-package*
+      (cdr (assoc *package* *lisp-package-web-package-alist*))))
+
 (defmacro web-defun (description web-lambda-list &body body)
 ;;   "Defines a function intended to be called over the web (though it can be called
 ;; as if it is a normal function, too.
 ;; 
 ;; DESCRIPTION is either a symbol or a list of the form:
-;; (name )
+;; (name &key content-type)
 ;; 
 ;; LAMBDA-LIST is a list where each elemtn is either a symbol VAR or a list of the form:
 ;; (var &key init-form parameter-name parameter-type)
 ;; 
 ;; description := name | name-lambda-list."
+
   
   ;; Standardize the description argument into the list form
   (when (atom description) (setf description (list description)))
   ;; Standardize the web-lambda-list items into lists
   (setf web-lambda-list (mapcar #'(lambda (x) (if (atom x) (list x) x)) web-lambda-list))
-  (let* ((fn-name (if (consp description) (first description) description))
-	 (parameter-names (mapcar #'first web-lambda-list))
-	 (make-parameters-form
-	  `(list ,@(mapcar #'(lambda (param-form)
-			       (destructuring-bind (name &key initform parameter-name parameter-type)
-				   param-form
-				 (declare (ignore initform parameter-name))
-				 `(make-instance 'web-function-parameter
-						 :name ',name
-						 :parameter-type ,parameter-type)))
-			   web-lambda-list)))
+  (destructuring-bind (fn-name &key content-type)
+      description
+    (let* ((parameter-names (mapcar #'first web-lambda-list))
+	   (make-parameters-form
+	    `(list ,@(mapcar #'(lambda (param-form)
+				 (destructuring-bind (name &key initform parameter-name parameter-type)
+				     param-form
+				   (declare (ignore initform parameter-name))
+				   `(make-instance 'web-function-parameter
+						   :name ',name
+						   :parameter-type ,parameter-type)))
+			     web-lambda-list)))
 
-	 (generic-lambda-list (cons '&key parameter-names))
-	 (method-lambda-list (cons '&key parameter-names))
+	   (generic-lambda-list (append (list '&key) parameter-names (list '&allow-other-keys)))
+	   (method-lambda-list (append (list '&key) parameter-names (list '&allow-other-keys)))
 
-	 (%fn (gensym "function"))
-	 (%parameters (gensym "parameters")))
-    `(eval-when (:compile-toplevel :load-toplevel :execute)
-       (let* ((*web-package* (or *web-package*
-				 (cdr (assoc *package* *lisp-package-web-package-alist*))))
-	      (,%parameters ,make-parameters-form)
-	      (,%fn
-	       (defgeneric ,fn-name ,generic-lambda-list 
-		 (:generic-function-class web-function))))
+	   (%fn (gensym "function"))
+	   (%parameters (gensym "parameters"))
+	   (content-type (and content-type (stringify-content-type content-type))))
+      `(eval-when (:compile-toplevel :load-toplevel :execute)
+	 (let* ((*web-package* (sane-web-package))
+		(,%parameters ,make-parameters-form)
+		(,%fn
+		 (defgeneric ,fn-name ,generic-lambda-list 
+		   (:generic-function-class web-function))))
 
-	 (setf (web-function-parameters ,%fn) ,%parameters)
+	   (setf (web-function-parameters ,%fn) ,%parameters
+		 (web-package ,%fn) *web-package*)
 
-	 (defmethod ,fn-name ,method-lambda-list
-	   ,@body)
-	 (pushnew ,%fn (web-package-functions *web-package*))
-	 ,%fn))))
+	   (defmethod ,fn-name ,method-lambda-list
+	     ,@(when content-type
+		 (list `(when (boundp 'hunchentoot:*reply*)
+			  (setf (hunchentoot:content-type) ,content-type))))
+	     ,@body)
+	   (pushnew ,%fn (web-package-functions *web-package*))
+	   ,%fn)))))
        ;; unfinished implementation
 
 (defun string-prefixp (string prefix)
@@ -295,8 +393,9 @@ INIT-FORM-FUNCTION"
 (defmethod web-package-prefix-matches-uri? ((web-package web-package) uri)
   "Returns 2 values.  1. nil if does not match, the matched prefix if it does match.
 2.  if it does match, the rest of the uri string."
-  (cl-ppcre:register-groups-bind (firstlevel secondlevel)
-				 ("/([^\\/]*)\\/?([^\\#\\?]*)" uri)
+  (cl-ppcre:register-groups-bind (firstlevel slash secondlevel postfix)
+				 ("/([^\\/]*)(\\/)?([^\\#\\?\\/]*)?(/[^\\#\\?]*)?" uri)
+    (declare (ignore slash))
     (let* ((uri-package-name (string-upcase (hunchentoot:url-decode firstlevel)))
 	   (matching-prefix (find-if #'(lambda (package-uri)
 					 (string-prefixp uri package-uri))
@@ -304,9 +403,9 @@ INIT-FORM-FUNCTION"
 					   (web-package-uri-aliases web-package)))))
     (cond 
       (matching-prefix
-       (values matching-prefix secondlevel))
+       (values matching-prefix secondlevel postfix))
       ((equal uri-package-name (symbol-name (web-package-name web-package)))
-       (values (string-downcase uri-package-name) secondlevel))
+       (values (string-downcase uri-package-name) secondlevel postfix))
       (t (values nil))))))
     
 
@@ -331,12 +430,17 @@ INIT-FORM-FUNCTION"
 (defparameter *wiretap* *standard-output*)
 
 (defmethod web-function-transform-request-into-arguments ((fn web-function) request)
-  (mapcan #'(lambda (fn-param)
-	      (list (intern (symbol-name (parameter-name fn-param)) :keyword)
-		    (compute-parameter (parameter-uri-name fn-param)
-				       (or (parameter-type fn-param) 'string)
-				       :both)))
-	  (web-function-parameters fn)))
+  `(:postfix
+    ,(multiple-value-bind (prefix fn-name-string postfix) 
+	 (web-package-prefix-matches-uri? (web-package fn) (hunchentoot:request-uri request))
+       (declare (ignore prefix fn-name-string))
+       postfix)
+    ,@(mapcan #'(lambda (fn-param)
+		  (list (intern (symbol-name (parameter-name fn-param)) :keyword)
+			(compute-parameter (parameter-uri-name fn-param)
+					   (or (parameter-type fn-param) 'string)
+					   :both)))
+	      (web-function-parameters fn))))
     
 
 
@@ -345,15 +449,25 @@ INIT-FORM-FUNCTION"
 
 (defmethod web-package-handler-for-request ((web-package web-package) request)
   (let ((uri (hunchentoot:request-uri request)))
-    (multiple-value-bind (prefix rest) 
+    (multiple-value-bind (prefix fn-name-string postfix) 
 	(web-package-prefix-matches-uri? web-package uri)
+      (declare (ignore postfix))
       (when prefix
-	(let* ((fn-name-string (string-upcase rest))
-	       (fn (find fn-name-string (web-package-functions web-package)
-			  :key #'(lambda (x) (symbol-name (web-function-name x))) :test #'equal)))
+	(let* ((fn-name-string (and fn-name-string (string-upcase fn-name-string)))
+	       (fn (if (and fn-name-string (< 0 (length fn-name-string)))
+		       (find fn-name-string
+			     (web-package-functions web-package)
+			     :key #'(lambda (x) (symbol-name (web-function-name x)))
+			     :test #'equal)
+		       (find-web-function web-package (web-package-root-function-name web-package)))))
 	  (if fn
-	      #'(lambda () (web-function-call-with-request fn request))
-	      #'(lambda () (format nil "~A ~A" fn-name-string (web-package-functions web-package)))))))))
+	      #'(lambda ()
+		  (let ((*web-package* web-package))
+		    (web-function-call-with-request fn request)))
+	      #'(lambda ()
+		  (format nil "~A ~A"
+			  fn-name-string
+			  (web-package-functions web-package)))))))))
 	      
 
 (defmethod web-package-handle-request ((web-package web-package) request)
@@ -497,10 +611,39 @@ REQUEST-TYPE is one of :GET, :POST, or :BOTH."
           (t (error "Don't know what to do with parameter type ~S." parameter-type)))))
 
 
+(defun href (fn-designator &optional (package *web-package*))
+  (web-function-href fn-designator package))
+
+(defun web-function-href (fn-designator &optional (package *web-package*))
+  (let ((fn (if (functionp fn-designator) fn-designator (fdefinition fn-designator))))
+    (format nil "/~A/~A"
+	    (string-downcase (web-package-name package))
+	    (string-downcase (web-function-name fn)))))
+
 
 ;(defun make-function-var-instance-from-var-definition-list (list)
 ;  (if (atom list)
 ;      (make-instance 'web-function-var :name list
  
 
-   
+(defun enough-url (url url-prefix)
+  "Returns the relative portion of URL relative to URL-PREFIX, similar
+to what ENOUGH-NAMESTRING does for pathnames."
+  (subseq url (or (mismatch url url-prefix) (length url-prefix))))
+
+(defun serve-static-file (given-path base-path &optional content-type)
+  (declare (optimize debug))
+  (let* ((given-path (subseq given-path 1))
+	 (script-path (ppcre:regex-replace-all "\\\\" given-path "/"))
+	 (script-path-directory (pathname-directory script-path)))
+    (unless (or (stringp script-path-directory)
+		(null script-path-directory)
+		(and (listp script-path-directory)
+		     (eq (first script-path-directory) :relative)
+		     (loop for component in (rest script-path-directory)
+                                    always (stringp component))))
+      (setf (hunchentoot:return-code) hunchentoot:+http-forbidden+)
+      (error "problem with ~S ~S" given-path script-path-directory)
+      (throw 'handler-done nil))
+      
+    (hunchentoot:handle-static-file (merge-pathnames script-path base-path) content-type)))
